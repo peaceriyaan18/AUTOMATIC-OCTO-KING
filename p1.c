1e06a82e5b859a724e04cbc89940f4fdde7e3b4c
peaceriyaan18 <59403226+peaceriyaan18@users.noreply.github.com>
2020-02-14 11:01:33 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,23 @@
+<html>
+ <head>
+        <title>
+        Marquee usage
+        </title>
+</head>
+<body>
+
+    <h3>using Marquee</h3>
+<marquee>
+      <p>An <em>essay</em> is a short piece of writing outlining
+      the writers perspective or story.<mark>It is often considered
+      synomous with a story or a paper or an article.</mark>
+      Essays can be formal as well as informal.<bdo dir="rtl">.
+      Formal essays are generally academic in nature and tackle
+      serious topics.</bdo> We will be focussing on informal essays
+      which are more personal and often have<strong> humorous elements.
+      </strong></p>
+
+<marquee>
+<marquee directions="up">
+<p>marquee going up
+         
